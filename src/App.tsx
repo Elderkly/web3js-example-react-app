dcc4651485aa449b4bd1d1a8ecbc798c88c78023
Elderkly <897676943@qq.com>
2024-06-06 13:57:12 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import {UtilExample} from './components/UtilExample'
 import {WalletExample} from './components/WalletExample'
 import logo from './logo.png'
+import { web3 } from './web3/web3';
 
 function App() {
+    useEffect(() => {
+        // 创建新的以太坊账户
+        const account = web3.eth.accounts.create();
+        
+        // 打印账户地址和私钥
+        console.log('Address:', account.address);
+        console.log('Private Key:', account.privateKey);
+    }, [])
     return (
         <div>
-            <div className='Header'>
+            {/* <div className='Header'>
                 <img src={logo} alt='logo'/>
-            </div>
+            </div> */}
             <div>
-                <div className='Card'><h1>Example React App</h1></div>
-                <UtilExample/>
+                {/* <div className='Card'><h1>Example React App</h1></div> */}
+                {/* <UtilExample/> */}
                 <WalletExample/>
             </div>
 
